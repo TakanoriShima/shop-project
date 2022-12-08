2a84756fc8290355de4dddab509f5022422417a0
TakanoriShima <quark2galaxy@gmail.com>
2022-12-08 12:53:40 +0900

    bootstrap

--- src/components/pages/Top.vue
+++ src/components/pages/Top.vue
@@ -2,13 +2,25 @@
 </script>
 
 <template>
-  <p>Top</p>
-  <img src="../../assets/images/1.jpg">
-  <router-link to="/menu">Menu</router-link>
+  <div class="container">
+    <div class="row">
+      <h1 class="text-center offset-sm-3 col-sm-6 text-danger">Top</h1>
+    </div>
+    <div class="row mt-2">
+      <div class="col-sm-6"><img src="../../assets/images/1.jpg"></div>
+      <div class="col-sm-6">
+        <a href="/menu">Menu2</a>
+        <router-link to="/menu">Menu</router-link>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
  p{
   color: red;
+ }
+ img {
+  width: 100%;
  }
 </style>
